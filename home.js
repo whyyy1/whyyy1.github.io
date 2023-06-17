1e5589571e1c98546e6b47a698eebd0b33645f66
whyyy1 <dysarttieran@gmail.com>
2023-06-16 19:38:41 -0600

    created addListner to load the pages and hide the pages when the buttons are clicked

--- home.js
+++ home.js
@@ -1,8 +1,11 @@
 
 
 
+
 //Event listener when you click the middle of the screen
+let showing = false;
 const middleClick = document.querySelector('.Middle');
+
 const colorList = ['red', 'green', 'blue', 'grey', 'purple', 'yellow', 'orange', 'pink', 'brown', 'cyan', 'magenta', 'teal', 'lime', 'olive', 'navy', 'maroon', 'gold', 'silver', 'indigo', 'violet', 'turquoise', 'coral', 'salmon', 'orchid', 'peru'];
 const randomSentences = [
     "<h3>This is a placeholder text</h3><span></span><p>Lorem ipsum dolor sit amet.</p>",
@@ -10,10 +13,10 @@ const randomSentences = [
     "<h3>Here's an interesting fact to include</h3><span></span><p>Lorem ipsum dolor sit amet.</p>",
     "<h3>Add a relevant image here</h3><span></span><p>Lorem ipsum dolor sit amet.</p>",
     "<h3>Don't forget to cite your sources</h3><span></span><p>Lorem ipsum dolor sit amet.</p>"
-  ];
+];
 
 middleClick.addEventListener('click', () => {
-    
+
     let randomColor = colorList[Math.floor(Math.random() * colorList.length)]
     middleClick.style.backgroundColor = randomColor;
 
@@ -25,14 +28,14 @@ let lastKnownScrollPosition = 0;
 let ticking = false
 
 addData.addEventListener('click', () => {
-    const sections = document.querySelectorAll('.sectionClass'); 
+    const sections = document.querySelectorAll('.sectionClass');
     console.log(sections)
-    
-    
+
+
     let randomColor = colorList[Math.floor(Math.random() * colorList.length)]
     let randomSentence = randomSentences[Math.floor(Math.random() * randomSentences.length)]
     middleClick.style.color = randomColor;
-    for(i of sections){
+    for (i of sections) {
         let addHeader = document.createElement('h3')
         addHeader.innerHTML = randomSentence
         addHeader.classList.add('h3Added')
@@ -41,39 +44,84 @@ addData.addEventListener('click', () => {
 
     }
 
-    
+
 });
 
 const delData = document.querySelector('.delData');
 
 delData.addEventListener('click', () => {
-    const sections = document.querySelectorAll('.sectionClass'); 
+    const sections = document.querySelectorAll('.sectionClass');
     let delData = document.querySelector('.h3Added')
     console.log(delData)
-    for(i of sections){
+    for (i of sections) {
         delData.remove()
         console.log(i)
     }
-    
+
 })
 
 
-const anchorLinks = document.querySelectorAll('.navLinks')
+//button listeners 
 
+const linkShowing = document.querySelectorAll('.lc')
+const aboutBtn = document.querySelector('.aboutLink')
+const projectBtn = document.querySelector('.projectLink')
+const contactBtn = document.querySelector('.contactLink')
 
-document.addEventListener('click', (event) => {
-        
-    const targetText = event.target.text
-    console.log(targetText)
-    if(targetText === 'About Me'){
-        console.log( document.querySelector("#aboutSec").style.display)
-        if (document.querySelector("#aboutSec").style.display === "none") {
-            document.querySelector("#aboutSec").style.display = "block";
-          } else {
-            document.querySelector("#aboutSec").style.display = "none";
-          }
-        
-    }
-    else if(targetText === 'Projects'){document.querySelector("#projectSec").style.display = 'inline-block'}
-    else if(targetText === 'Contact'){document.querySelector("#contactSec").style.display = 'inline-block'}
-})
+for (i of linkShowing) {
+
+    i.addEventListener('click', (event) => {
+        // console.log(document.querySelector('#aboutSec').innerHTML)
+        let section;
+        let id;
+        if (event.target.textContent === 'About Me') {
+            section = '#aboutSec'
+            id = 'aboutSec'
+
+        }
+        else if (event.target.textContent === 'Projects') {
+            section = '#projectSec';
+            id = 'projectSec'
+        }
+        else if (event.target.textContent === 'Contact') {
+            section = '#contactSec'
+            id = 'contactSec'
+        }
+        for (j of document.querySelector('.Middle').children) {
+
+            if (j.classList.value === 'active') {
+                console.log(j.classList.value)
+                console.log(j, id)
+                if (j.id !== id) {
+                    j.classList.value = ''
+                    j.style.display = 'none';
+                    showing = false
+                }
+            }
+        }
+        if (!showing && (document.querySelector(section).style.display === 'none' || document.querySelector(section).style.display === '')) {
+
+            document.querySelector(section).style.display = 'block'
+            document.querySelector(section).classList.add('active')
+            showing = true
+
+        }
+
+        // console.log(document.querySelector('#aboutSec').style.display,showing)
+        else if (showing) {
+            if ((document.querySelector(section).style.display === '' || document.querySelector(section).style.display === 'block')) {
+                document.querySelector(section).style.display = 'none'
+                showing = false
+            }
+
+            // event.target.querySelector(section).style.display = 'none'
+            // showing = false
+        }
+        // console.log(section,document.querySelector(section).style.display,showing)
+        // console.log(document.querySelector(section))
+
+    })
+
+}
+const aboutMark = `<h1>This is the About me section</h1><p>My name is Tieran Dysart and I am a self taught programmer. Currently in a 15 week bootcamp to become a Software Engineer`
+document.querySelector('#aboutSec').innerHTML = aboutMark
--- main.js
+++ main.js
@@ -1,64 +1,145 @@
 const mainNav = document.querySelector('.mainParent');
 const mainBtn = document.querySelector('.btn1');
 
+
+const COLORS = ["red", "orange", "yellow", "green", "blue",
+  "indigo", "violet", "pink", "purple", "brown",
+  "black", "white", "gray", "silver", "gold",
+  "maroon", "navy", "teal", "olive", "coral",
+  "magenta", "lime", "cyan", "peach", "turquoise"]
+const PRESET = [
+  "position",
+  "height",
+  "width",
+  "bottom",
+  "right",
+  "border-radius",
+  
+]
+const style1 = [
+   "absolute",
+   "17vw",
+   "18vw",
+   '5vw',
+    '0vw',
+    '50%',
+]
+const style2 = [
+  "absolute",
+  "7vw",
+  "8vw",
+  '15vw',
+   '0vw',
+   '50%',
+]
+const style3 = [
+  "absolute",
+  "10vw",
+  "11vw",
+  '5vw',
+   '0vw',
+   '50%',
+]
+const style4 = [
+  "absolute",
+  "17vw",
+  "18vw",
+  '5vw',
+   '0vw',
+   '50%',
+]
+const style5 = [
+  "absolute",
+  "17vw",
+  "18vw",
+  '5vw',
+   '0vw',
+   '50%',
+]
 //click listens 
 
 mainNav.addEventListener('click', (event) => {
-    console.log(event.target);
+  console.log(event.target);
 })
 
 mainBtn.addEventListener('click', () => {
-    console.log(mainNav);
+  
+  let total = (mainNav.children.length) + 1;
+  const newDiv = document.createElement('div')
+  newDiv.setAttribute('class', `class${total}`)
+  let ranAng = Math.floor(Math.random() * 6)
+  let ranRad = Math.floor(Math.random() * 800)
+  let ranIntv = Math.floor(Math.random() * 4)
+  let ranRot = Math.floor(Math.random() * 2)
+  console.log(ranAng, ranRad, ranIntv, ranRot)
+  
+  if (ranRot === 0) {
+    myMove(newDiv.className, ranAng, ranRad, ranIntv)
+  }
+  if (ranRot === 1) {
+    myMoveReverse(newDiv.className, ranAng, ranRad, ranIntv)
+  }
+  for(i in PRESET){
+    let mainPreset  = PRESET[i]
+    // console.log(newDiv.style.mainPreset)
+    console.log(newDiv.mainPreset)
+    // newDiv.mainPreset  = `"${style1[i]}"`
+    // console.log(newDiv.mainPreset)
+  }
+  // console.log(newDiv.style.position)
+  // mainNav.appendChild(newDiv)
+  
+  
 })
 
 
 function myMove(name, ang = 0, rad = 100, inT) {
 
-    let id = null;
-    const elem = document.querySelector(name);
-    let angle = ang;
-    let radius = rad; // Adjust the radius of the circular path
-    clearInterval(id);
-    id = setInterval(frame, inT);
+  let id = null;
+  const elem = document.querySelector(name);
+  let angle = ang;
+  let radius = rad; // Adjust the radius of the circular path
+  clearInterval(id);
+  id = setInterval(frame, inT);
 
-    function frame() {
-        if (angle >= 360) {
-            angle = 0;
-        } else {
-            angle++;
-            let radians = angle * (Math.PI / 180);
-            let x = Math.cos(radians) * radius;
-            let y = Math.sin(radians) * radius;
+  function frame() {
+    if (angle >= 360) {
+      angle = 0;
+    } else {
+      angle++;
+      let radians = angle * (Math.PI / 180);
+      let x = Math.cos(radians) * radius;
+      let y = Math.sin(radians) * radius;
 
-            elem.style.left = x + 'px';
-            elem.style.top = y + 'px';
-        
+      elem.style.left = x + 'px';
+      elem.style.top = y + 'px';
+
 
     }
-}
+  }
 }
 
 function myMoveReverse(name, ang = 0, rad = 100, inT) {
-    let id = null;
-    const elem = document.querySelector(name);
-    let angle = ang;
-    let radius = rad; // Adjust the radius of the circular path
-    clearInterval(id);
-    id = setInterval(frame, inT);
-  
-    function frame() {
-      if (angle <= 0) {
-        angle = 360;
-      } else {
-        angle--;
-        let radians = angle * (Math.PI / 180);
-        let x = Math.cos(radians) * radius * -1; // Multiply with -1 to reverse the x-coordinate
-        let y = Math.sin(radians) * radius * -1; // Multiply with -1 to reverse the y-coordinate
-  
-        elem.style.left = x + 'px';
-        elem.style.top = y + 'px';
-      }
+  let id = null;
+  const elem = document.querySelector(name);
+  let angle = ang;
+  let radius = rad; // Adjust the radius of the circular path
+  clearInterval(id);
+  id = setInterval(frame, inT);
+
+  function frame() {
+    if (angle <= 0) {
+      angle = 360;
+    } else {
+      angle--;
+      let radians = angle * (Math.PI / 180);
+      let x = Math.cos(radians) * radius * -1; // Multiply with -1 to reverse the x-coordinate
+      let y = Math.sin(radians) * radius * -1; // Multiply with -1 to reverse the y-coordinate
+
+      elem.style.left = x + 'px';
+      elem.style.top = y + 'px';
     }
+  }
 }
 
 
@@ -71,5 +152,5 @@ myMoveReverse('.class2', 5, 400, 5);
 myMove('.class3', 8, 100, 4);
 myMoveReverse('.class4', 5, 500, 6);
 myMove('.class5', 6, 300, 1);
-myMove('.class6', 1,600, 10);
-myMoveReverse('.class7', 40, 250, 14);
+myMove('.class6', 1, 600, 10);
+myMoveReverse('.class7', 4, 250, 5);
